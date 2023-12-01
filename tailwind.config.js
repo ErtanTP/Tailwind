e96a3a69fa2a34a8cf46a51718cb72a32cf2c99a
ertan.aydin <ertanaydin@NB.188>
2023-12-01 14:18:51 +0100

    better

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,16 @@ module.exports = {
   mode:"jit",
   content: [__dirname+"/out/**/*.{html,js}"],
   theme: {
+    container: {
+      padding: "10px",
+      center: true,
+      screens: {
+        sm: '100%',
+        md: '100%',
+        lg: '768px',
+        
+      },
+    },
     extend: {},
   },
   plugins: [], 
